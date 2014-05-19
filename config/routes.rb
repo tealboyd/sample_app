b94502e3111172f968bcd90f79eb6a371d0cd37a
Gerry Boyd <tealboyd@gmail.com>
2014-05-19 19:06:25 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  
+  get "users/new"
+  root 'static_pages#home'
+  #get "static_pages/help"
+  #get "static_pages/about"
+  #get "static_pages/contact"
+  
+  match '/signup', to: 'users#new', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,4 +65,6 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,60 +2,76 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  subject { page }
+  
   describe "Home page" do
+    before { visit root_path }
     # Content
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    it { should have_content('Sample App') }
     # Title
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("RoR Tutorial Sample App")
-    end
-    it "should not have a custom page title"  do
-      visit '/static_pages/home'
-      expect(page).not_to have_title(' | Home')
-    end        
+    it { should have_title(full_title('')) }
+    it { should_not have_title(' | Home') }
+    #it "should have the content 'Sample App'" do
+      #expect(page).to have_content('Sample App')
+      #end
+    # Title
+    #it "should have the base title" do
+      #expect(page).to have_title("RoR Tutorial Sample App")
+      #end
+    #it "should not have a custom page title"  do
+      #expect(page).not_to have_title(' | Home')
+      #end        
   end
   
   describe "Help page" do
+    before { visit help_path }
     # Content
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    it { should have_content('Help') }
+    #Title
+    it { should have_title(full_title('Help')) }
+    #it "should have the content 'Help'" do
+      #visit help_path
+      #expect(page).to have_content('Help')
+      #end
     # Title
-    it "should have the right title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("RoR Tutorial Sample App | Help")
-    end                
+    #it "should have the right title 'Help'" do
+      #visit help_path
+      #expect(page).to have_title("RoR Tutorial Sample App | Help")
+      #end                
   end
   
   describe "About page" do
+    before { visit about_path }
     #Content
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    it { should have_content('About Us') }
+    #Title
+    it { should have_title(full_title('About Us')) }
+    #it "should have the content 'About Us'" do
+      #visit about_path
+      #expect(page).to have_content('About Us')
+      #end
     # Title
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title('RoR Tutorial Sample App | About Us')
-    end              
+    #it "should have the right title" do
+      #visit about_path
+      #expect(page).to have_title('RoR Tutorial Sample App | About Us')
+      #end              
   end
   
   describe "Contact page" do
+    before { visit contact_path }
     #Content
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
+    it { should have_content('Contact') }
+    #Title
+    it { should have_title(full_title('Contact')) }
+    #it "should have the content 'Contact'" do
+      #visit contact_path
+      #expect(page).to have_content('Contact')
+      #end
     # Title
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      expect(page).to have_title('RoR Tutorial Sample App | Contact')
-    end              
+    #it "should have the right title" do
+      #visit contact_path
+      #expect(page).to have_title('RoR Tutorial Sample App | Contact')
+      #end              
   end
   
   
